8c8568dda188f3dfcbbf3643a6ce5e800c307f68
Thiago Roberto <thiago.nemo4code@gmail.;com>
2020-04-20 20:09:28 -0300

    adicionado model e migration de Tutor

---
+++ src/db/migrations/20200415010820-create-tutor.js
@@ -0,0 +1,62 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Tutors", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      rg: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      cpf: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+      phone: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      whatsapp: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      cep: {
+        type: Sequelize.STRING(8),
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: Sequelize.STRING,
+      area: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Tutors");
+  },
+};
