b6b05732d99a02f29788299cc7189f264305c324
LRabelo <47678024+LRabelo@users.noreply.github.com>
2020-09-14 21:31:20 -0300

    Swagger
    Documentação de API

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,9 +1,12 @@
 import request from "supertest";
-import server from "../server";
+
+afterAll(() => {
+  server.close();
+});
 
 describe("initial tests", () => {
-  test("api route", () => {
-   
-    expect(1).toEqual(1);
+  test("api route", async () => {
+    const response = await request(server).get("/");
+    expect(response.status).toEqual(200);
   });
 });
