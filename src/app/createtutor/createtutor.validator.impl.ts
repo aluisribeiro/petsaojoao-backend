0ca651fa1ee6a536849919fc8bbbda8bfae0d4b5
Fernando Andrade <fernando.c.andrade@sou.unifeob.edu.br>
2020-10-20 21:15:16 -0300

    Add feature validation implements

--- src/app/createtutor/createtutor.validator.impl.ts
+++ src/app/createtutor/createtutor.validator.impl.ts
@@ -1,10 +1,52 @@
-import { ValidatorResult } from '../core/definitions/validator-result';
-import { CreateTutorValidator } from './createtutor.validator';
-import { CreateTutorInput } from './createtutor.in';
+import { ValidatorResult } from "../core/definitions/validator-result";
+import { CreateTutorValidator } from "./createtutor.validator";
+import { CreateTutorInput } from "./createtutor.in";
 
-export class CreateTutorValidatorImpl implements CreateTutorValidator{
+import Ajv from "ajv";
+const ajv = new Ajv();
 
-  validate(request: CreateTutorInput): ValidatorResult{
+export class CreateTutorValidatorImpl implements CreateTutorValidator {
+  validate(request: CreateTutorInput): ValidatorResult {
+    const schema = {
+      properties: {
+        name: {
+          type: "string",
+          maxLength: 80,
+        },
+        email: {
+          type: "string",
+          format: "email",
+          maxLength: 100,
+        },
+        rg: { type: "string", maxLength: 15, unique: true },
+        cpf: {
+          type: "string",
+          maxLength: 11,
+          pattern: "[0-9]{11}",
+          unique: true,
+        },
+        phone: { type: "string", maxLength: 11 },
+        whatsapp: { type: ["string", "null"], maxLength: 11 },
+        cep: {
+          type: "string",
+          maxLength: 8,
+          minLength: 8,
+          pattern: "13870*",
+        },
+        street: { type: "string", maxLength: 100 },
+        number: { type: ["string", "null"], maxLength: 30 },
+        area: { type: ["string", "null"], maxLength: 100 },
+        complement: {
+          type: ["string", "null"],
+          maxLength: 100,
+        },
+      },
+      required: ["name", "email", "rg", "cpf", "cep", "street"],
+    };
+
+    let validation = ajv.validate(schema, request);
+
+    if (!validation) return { valid: false, error: {} };
 
     return { valid: true, error: {} };
   }
