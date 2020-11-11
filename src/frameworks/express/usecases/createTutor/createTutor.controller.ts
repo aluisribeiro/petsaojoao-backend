7abf65fa13f6251855c3277ffc3df05ddc16da3f
Potter713 <cesar.simionato@sou.unifeob.edu.br>
2020-11-10 22:00:53 -0300

    create tutor test

--- src/frameworks/express/usecases/createTutor/createTutor.controller.ts
+++ src/frameworks/express/usecases/createTutor/createTutor.controller.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from 'express'
 
-// import { CreateTutorInput } from '../../../../app/createtutor/createtutor.in';
-// import { CreateTutorInteractor } from '../../../../app/createtutor/createturor.interactor'
-// import { CreateTutorValidator } from '../../../../app/createtutor/createtutor.validator';
-
 import { CreateTutorInput } from '@/app/createtutor/createtutor.in';
 import { CreateTutorInteractor } from '@/app/createtutor/createturor.interactor'
 import { CreateTutorValidator } from '@/app/createtutor/createtutor.validator';
+import { Json } from 'sequelize/types/lib/utils';
 
 class CreateTutorController {
-  async create (request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     try {
+
+      try {
+        JSON.stringify(request.body);
+      } catch (error) {
+        return response.status(400).json({
+          message: 'invalid data format',
+          error
+        })
+      }
+
       const {
         name,
         email,
@@ -24,7 +31,13 @@ class CreateTutorController {
         area,
         complement,
       } = request.body;
-    
+
+      if (!name || !email || !rg || !cpf || !phone || !cep) {
+        return response.status(400).json({
+          error: "mandatory fields not filled"
+        })
+      }
+
       const tutor: CreateTutorInput = {
         name,
         email,
@@ -38,34 +51,33 @@ class CreateTutorController {
         area,
         complement,
       }
-    
+
       let validator: CreateTutorValidator;
       let errorFactory;
       let repository;
       let interactor: CreateTutorInteractor;
-    
+
       validator = {
         validate: () => {
           return { valid: true, error: {} };
         },
       };
-    
+
       errorFactory = {
         getError: () => new Error('test'),
       };
-    
+
       repository = {
         saveTutor: async () => Promise.resolve(true),
       };
-    
+
       interactor = new CreateTutorInteractor(
         validator,
         errorFactory,
         repository
       );
-    
+
       const out = await interactor.execute(tutor);
-    
       response.json(out);
     } catch (error) {
       return response.status(400).json({
--- src/frameworks/express/usecases/createTutor/createTutor.spec.ts
+++ src/frameworks/express/usecases/createTutor/createTutor.spec.ts
@@ -1,34 +1,51 @@
-import { CreateTutorInput } from '@/app/createtutor/createtutor.in';
-import { CreateTutorInteractor } from '@/app/createtutor/createturor.interactor'
-import { CreateTutorValidator } from '@/app/createtutor/createtutor.validator';
-
-const request = new Promise((resolve, reject) => {
-  try {
-    resolve(
-      {
-        "name": "cesar",
-        "email": "cesar@hotmail.com",
-        "rg": "12.345.678-9",
-        "cpf": "123.456.789-10",
-        "phone": "123456789",
-        "cep": "13990000",
-        "whatsapp": "987654321",
-        "street": "Rua dos Alfeneiros",
-        "number": "04",
-        "area": "Surrey",
-        "complement": ""
-      }
-    )
-  } catch (e) {
-    reject(e)
-  }
-})
+import request from 'supertest'
+import app from "@/frameworks/express/index";
 
 describe('CreateTutor', () => {
 
-  test('If json is valid', async () => {
-    const tutor = await request;
-    // Nao sei como faz para seber se é um json, typeof talvez?
-    expect(true).toEqual(true);
-  })
+  test('should return error if the data format is not json', async () => {
+    await request(app)
+      .post('/createTutor')
+      .send("<?xml version='1.0'><filmes><filme id='1'><titulo>O XML veste prada</titulo><resumo>O filme mostra a elegância da XML na representação de dados estruturados e semi estruturados.</resumo><genero>Aventura</genero><genero>Documentário</genero><elenco><ator>Mark UPlanguage</ator><ator>Mary well-Formed</ator><ator>Sedna D. Atabase</ator></elenco></filme></filmes>")
+      .expect(400)
+  }, 2000)
+
+  test('should return error if any mandatory fields are not filled', async () => {
+    await request(app)
+      .post('/createTutor')
+      .send({
+        name: "",
+        email: "cesar@hotmail.com",
+        rg: "12.345.678-9",
+        cpf: "123.456.789-10",
+        phone: "123456789",
+        cep: "13990000",
+        whatsapp: "987654321",
+        street: "Rua dos Alfeneiros",
+        number: "04",
+        area: "Surrey",
+        complement: ""
+      })
+      .expect(400)
+  }, 2000)
+
+  test('should return success', async () => {
+    await request(app)
+      .post('/createTutor')
+      .send({
+        name: "cesar",
+        email: "cesar@hotmail.com",
+        rg: "12.345.678-9",
+        cpf: "123.456.789-10",
+        phone: "123456789",
+        cep: "13990000",
+        whatsapp: "987654321",
+        street: "Rua dos Alfeneiros",
+        number: "04",
+        area: "Surrey",
+        complement: ""
+      })
+      .expect(200)
+  }, 2000)
+
 })
