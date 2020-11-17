bdf99e02f804ded8ca6399a6bc99042cd1ee8ea8
Potter713 <cesar.simionato@sou.unifeob.edu.br>
2020-11-16 21:19:24 -0300

    test 4

--- src/frameworks/express/usecases/createTutor/createTutor.spec.ts
+++ src/frameworks/express/usecases/createTutor/createTutor.spec.ts
@@ -1,16 +1,17 @@
-import request from 'supertest'
+import supertest from 'supertest'
 import app from "@/frameworks/express/index";
 
 describe('CreateTutor', () => {
-  test('should return error if the data format is not json', async () => {
-    await request(app)
+  test('should return error if the data format is not json', async (done) => {
+    await supertest(app)
       .post('/createTutor')
       .send("<?xml version='1.0'><filmes><filme id='1'><titulo>O XML veste prada</titulo><resumo>O filme mostra a elegância da XML na representação de dados estruturados e semi estruturados.</resumo><genero>Aventura</genero><genero>Documentário</genero><elenco><ator>Mark UPlanguage</ator><ator>Mary well-Formed</ator><ator>Sedna D. Atabase</ator></elenco></filme></filmes>")
       .expect(400)
+      done();
   })
 
-  test('should return error if any mandatory fields are not filled', async () => {
-    await request(app)
+  test('should return error if any mandatory fields are not filled', async (done) => {
+    await supertest(app)
       .post('/createTutor')
       .send({
         name: "",
@@ -26,10 +27,11 @@ describe('CreateTutor', () => {
         complement: ""
       })
       .expect(400)
+      done();
   })
 
-  test('should return success', async () => {
-    await request(app)
+  test('should return success', async (done) => {
+    await supertest(app)
       .post('/createTutor')
       .send({
         name: "cesar",
@@ -45,6 +47,7 @@ describe('CreateTutor', () => {
         complement: ""
       })
       .expect(200)
+      done();
   })
 
 })
