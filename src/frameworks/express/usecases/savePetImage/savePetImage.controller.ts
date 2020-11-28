5c2c6ec1d93a5bb95c9ceb7cf3825432cb895be3
Fernando Andrade <fernando.c.andrade@sou.unifeob.edu.br>
2020-11-28 18:36:29 -0300

    Add controller petImage

---
+++ src/frameworks/express/usecases/savePetImage/savePetImage.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+
+import { SavePetImageInput } from '@/app/savepetimage/savepetimage.in';
+import { SavePetImageInteractor } from '@/app/savepetimage/savepetimage.interactor';
+
+import { SavePetImageValidatorImpl } from '@/app/savepetimage/savepetimage.validator.impl';
+
+import { SavePetImagesImpl } from '@/frameworks/firebase/savepetimage/savepetimage.repository.impl';
+
+class SavePetImageController {
+  async create(request: Request, response: Response) {
+    try {
+
+      const savePetImageIn: SavePetImageInput = {
+        petId: request.body.petId,
+        petImage: request.file 
+      }
+
+      const validator = new SavePetImageValidatorImpl
+      const repository = new SavePetImagesImpl
+      const interactor = new SavePetImageInteractor(
+        validator,
+        repository
+      );
+
+      const out = await interactor.execute(savePetImageIn);
+      response.json(out);
+    } 
+    
+    catch (error) {
+      return response.status(400).json({
+        message: '',
+        error
+      })
+    }
+  }
+}
+
+export default SavePetImageController;
