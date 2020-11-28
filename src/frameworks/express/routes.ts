e5808226217c8411fd4d45584ee7d1f4d98395ca
Fernando Andrade <fernando.c.andrade@sou.unifeob.edu.br>
2020-11-28 18:35:27 -0300

    Add new route "/savepetimage"

--- src/frameworks/express/routes.ts
+++ src/frameworks/express/routes.ts
@@ -1,15 +1,25 @@
 import express from 'express'
 
 import CreateTutorController from './usecases/createTutor/createTutor.controller';
+import SavePetImageController from './usecases/savePetImage/savePetImage.controller';
+
+import multer from 'multer';
+import multerConfig from '@/frameworks/multer/config.ts';
+
+const uploadMulter = multer(multerConfig);
+
 
 const routes = express.Router()
 
 const createTutor = new CreateTutorController();
+const savePetImage = new SavePetImageController();
 
 routes.get('/', (request, response) => {
   return response.send(`Hi Cesar`);
 });
 
 routes.post('/createTutor', createTutor.create);
+
+routes.post('/savepetimage', uploadMulter.single('petImage'), savePetImage.create);
 
 export default routes
